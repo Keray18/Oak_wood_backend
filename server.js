2aec6944caf8abd3ccdcce2c6b28fedf2a18f55d
Keray18 <keshavbahoray18@gmail.com>
2025-01-04 01:23:03 +0530

    CORS origin modified

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(upload.none());
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: ['http://localhost:5173', 'https://oakwood18.netlify.app']
 }))
 
 app.use(session({
